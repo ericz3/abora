4c4675e03e870db38e647d430d0a83b8b2ca7b65
ericz3 <eric.fu.zhang@gmail.com>
2020-07-05 02:48:45 -0700

    header bar completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import logo from '../placeholdLogo.png';
 
 function Header(){
     return (
-        <header style = {headerStyle}>
-            Team Name Placeholder
+        <header style = {{width: '100%', height: '50px', background: '#5B818D',}}>
+            <div style = {headerLogoStyle}> 
+                <img src = {logo} alt = 'logo' style={logoImageStyle}></img>
+            </div>
+            <div style = {headerTitleStyle}>Team Name Placeholder</div>    
         </header>
     )
 }
 
-const headerStyle = {
+const headerLogoStyle = {
+    paddingLeft: '10px',
+    paddingTop: '6px',
+    float: 'left'
+}
+
+const logoImageStyle = {
+    width: '36px',
+    height: '36px',
+    borderRadius: '15%'
+}
+
+const headerTitleStyle = {
     fontSize: '22px',
     fontWeight: '400',
-    lineHeight: 2.5,
-    paddingLeft: '10px',
-    paddingRight: '10px',
-    background: '#5B818D',
-    color: '#FFFFFF'
+    color: '#FFFFFF',
+    paddingTop: '12px',
+    marginLeft: '58px'
 }
 
 export default Header;
