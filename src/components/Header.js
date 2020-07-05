4a2076f567df40fe46ba5b8db1745283ed80dc8e
ericz3 <eric.fu.zhang@gmail.com>
2020-07-05 00:34:20 -0700

    fix banner font-weight issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 function Header(){
     return (
-        <header className="Header-bar">
-            <h1>Team Name Placeholder</h1>
+        <header style = {headerStyle}>
+            Team Name Placeholder
         </header>
     )
+}
+
+const headerStyle = {
+    fontSize: '22px',
+    fontWeight: '400',
+    lineHeight: 2.5,
+    paddingLeft: '10px',
+    paddingRight: '10px',
+    background: '#5B818D',
+    color: '#FFFFFF'
 }
 
 export default Header;
