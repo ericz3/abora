6daa1ec35cf33c69105122939e21000e552598b1
ericz3 <eric.fu.zhang@gmail.com>
2020-07-08 19:11:33 -0700

    removed custom title bar, changed header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './components/Header';
-import MacTitleBar from './components/MacTitleBar';
 import './App.css';
 
 function App() {
   return (
     <body className="App">
-      <MacTitleBar />
       <Header />
     </body>
   );
---
+++ src/components/rooms/RoomsList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class RoomsList extends React.Component {
+    state = {
+        show: true
+    }
+}
