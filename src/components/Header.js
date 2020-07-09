ac77312884e43acb55cd935e517204c21e9e51f0
ericz3 <eric.fu.zhang@gmail.com>
2020-07-09 15:10:52 -0700

    moved styling from inline to internal css, cleaned up logic in render

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import logo from '../placeholdLogo.png';
+import './Header.css'
 
 class Header extends React.Component{
     render(){
-        const headerLogoStyle = {
-            float: 'left',
-            marginLeft: '14px',
-            marginTop: '8px',
-            width: '39px',
-            height: '39px',
-            borderRadius: '25%'
-        }
-        
-        const headerTitleStyle = {
-            fontSize: '20px',
-            fontWeight: '600',
-            color: '#454545',
-            paddingTop: '17px',
-            marginLeft: '64px',
-            letterSpacing: '1.2px'
-        }
-
         return (
-            <header style = {{width: '100%', height: '54px', background: '#FFFFFF',}}>
-                <img src = {logo} alt = 'logo' style={headerLogoStyle}></img>
-                <div style = {headerTitleStyle}>Temp Name</div>    
-            </header>
+            <div className='header-bar'>
+                <img src = {logo} alt='logo' className='logo'></img>
+                <div className='title'>Temp Name</div>    
+            </div>
         )
     }
 
