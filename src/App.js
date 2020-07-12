dfb39617717f45a3a52e992a4aa7d23266473443
Johnny Chang <yuchenc2@illinois.edu>
2020-07-12 02:28:21 -0500

    Fixed location issue

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component{
   render (){
     return (
       <div className='App'>
-        <div>
+        <div className='scroll-content'>
           <Video/>
         </div>
         <div className='video-control-footer'>
