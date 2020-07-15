a9cc1d658a627eeae4c3cc72b207b117e7cd1816
ericz3 <eric.fu.zhang@gmail.com>
2020-07-14 17:39:45 -0700

    sync-up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 import { HashRouter, Route, Redirect } from 'react-router-dom';
 import Login from './Login';
 
@@ -13,8 +12,3 @@ ReactDOM.render(
   </HashRouter>,
   document.getElementById('root')
 );
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
