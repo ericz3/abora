99e3c9eb03f33f2b16b8f6edca2803e4de2a70fa
ericz3 <eric.fu.zhang@gmail.com>
2020-07-14 23:53:12 -0700

    fix merge conflicts with master

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-// import Header from './components/Header';
-// import RoomsList from './components/RoomsList';
-// import RoomsSearchBar from './components/SearchBar';
+import React from 'react';
+import Header from './components/Header';
+import RoomsList from './components/RoomsList';
+import RoomsSearchBar from './components/SearchBar';
+import SimpleBar from 'simplebar-react';
 import Video from './components/Video';
 import VideoControl from './components/VideoControl';
+
+import "simplebar/src/simplebar.css";
 import './App.css';
 
 class App extends React.Component{
   render (){
     return (
       <div className='App'>
-        <div className='scroll-content'>
-          <Video/>
+        <div className='header-bar'>
+          <Header />
         </div>
-        <div className='video-control-footer'>
-          <VideoControl/>
-       </div>
+        <SimpleBar className='scroll-content'>
+            <RoomsList roomType={RoomsList.roomTypes.team}/>
+            <div className='line-horizontal-white'/>
+            <RoomsList roomType={RoomsList.roomTypes.personal}/>
+        </SimpleBar>
+        <div className='rooms-search-footer'>
+          <RoomsSearchBar />
+        </div>
       </div>
     );
   }
---
+++ src/components/RoomsList.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import {FaPlus} from 'react-icons/fa'
+import {FaCaretDown, FaCaretRight} from 'react-icons/fa'
+import Collapse from '@material-ui/core/Collapse';
+import './RoomsList.css'
+
+const roomTypes = {
+    personal: 'personal',
+    team: 'team'
+}
+
+class RoomsList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            show: true
+        };
+    }
+
+    toggleShow = () => {
+        this.setState(state => ({show : !state.show}));
+    };
+
+    createRoom = () => {
+        this.setState(state => ({show : !state.show}));
+    }
+
+    render() {
+        let caret = this.state.show 
+                    ? <FaCaretDown className='float-left'/> 
+                    : <FaCaretRight className='float-left'/>
+        let dropDownTitle = (this.props.roomType === roomTypes.team) 
+                    ? 'Team Rooms'
+                    : 'Personal Rooms'
+        return (
+            <div className='unselectable'>
+                <div className='drop-down' onClick={this.toggleShow}>
+                    {caret}
+                    <div className='drop-down-title'> 
+                        {dropDownTitle}
+                    </div>
+                    {this.props.roomType === roomTypes.team && 
+                        <div className='create-room-button' onClick={this.createRoom}>
+                           <FaPlus/>
+                        </div>
+                    }
+                </div>
+                <Collapse in={this.state.show} timeout={200} >
+                    <div style = {{height : '600px', background : '#ffffff'}}></div> 
+                </Collapse>
+            </div>
+        );
+    }
+}
+
+RoomsList.propTypes = {
+    roomType: propTypes.oneOf(Object.keys(roomTypes))
+}
+
+RoomsList.roomTypes = roomTypes;
+
+export default RoomsList;
