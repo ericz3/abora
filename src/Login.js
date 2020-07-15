0fcbb8967edfa810f9fd8f4e910a90aa9df79049
ericz3 <eric.fu.zhang@gmail.com>
2020-07-14 22:57:43 -0700

    links between login and home established

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import './Login.css'
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+const { BrowserWindow } = window.require('electron');
 
 class Login extends React.Component{
     render(){
         return (
             <div className='Login'>
-               
+                <Link 
+                    style = {{height: '40px', width: '40px', backgroundColor: '#454545'}} 
+                    to="/home">
+                        Login
+                </Link>
             </div>
         )
     }
-
 }
 
 export default Login;
